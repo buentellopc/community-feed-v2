cb675afbbc1080896f767210c14bd6050c36d9c6
buentellopc <buentellopc@gmail.com>
2022-09-27 23:47:37 -0500

    Configure next.js to use styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalyStyle = createGlobalStyle`
+  body { 
+       margin: 0; 
+       padding: 0; 
+       font-family: -apple-system, BlinkMacSystemFont,
+          "Segoe UI", "Roboto", "Oxygen","Ubuntu",  
+          "Cantarell", "Fira Sans", "Droid Sans",
+          "Helvetica Neue", sans-serif;  
+       -webkit-font-smoothing: antialiased; 
+       -moz-osx-font-smoothing: grayscale; 
+     } 
+   `;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalyStyle />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
